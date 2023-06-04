0be8269a2c2b31d96a6013b4f87771dff844a0e0
piyo <chick.p.1120@gmail.com>
2023-06-04 10:48:23 +0900

    refactor: extract function for sending everything-ics

--- src/bg/index.ts
+++ src/bg/index.ts
@@ -1,16 +1,30 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.storage.local.get("url").then(({ url: everthingIcsBaseUrl }) => {
-    if (!everthingIcsBaseUrl) {
-      chrome.tabs.create({
-        url: "chrome://extensions/?options=" + chrome.runtime.id,
-      });
-      return;
-    }
-    const contentPageUrl = tab.url;
-    if (contentPageUrl) {
-      chrome.tabs.create({
-        url: `${everthingIcsBaseUrl}/ics?url=${contentPageUrl}`,
-      });
-    }
+const getEverythingIcsBaseUrl = () => {
+  const key = "url";
+  return new Promise<string>((resolve) => {
+    chrome.storage.local.get(key, (items) => {
+      const url = items[key];
+      if (url) {
+        resolve(items[key]);
+      } else {
+        chrome.tabs.create({
+          url: "chrome://extensions/?options=" + chrome.runtime.id,
+        });
+      }
+    });
   });
+};
+
+const sendToEverthingIcs = (everythingIcsBaseUrl: string, url: string) => {
+  chrome.tabs.create({
+    url: `${everythingIcsBaseUrl}/ics?url=${url}`,
+  });
+};
+
+chrome.action.onClicked.addListener(async (tab) => {
+  const everythingIcsBaseUrl: string = await getEverythingIcsBaseUrl();
+  console.log(everythingIcsBaseUrl);
+  const url = tab.url;
+  if (everythingIcsBaseUrl && url) {
+    sendToEverthingIcs(everythingIcsBaseUrl, url);
+  }
 });
